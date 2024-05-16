e8a3d8b576da39808efea95e852e8f6c14e225a6
Wallace Andrade <andradewall0@gmail.com>
2024-05-16 12:23:38 -0300

    send files

--- .wezterm.lua
+++ .wezterm.lua
@@ -7,4 +7,7 @@ config.hide_tab_bar_if_only_one_tab = true
 config.use_fancy_tab_bar = false
 config.tab_bar_at_bottom = true
 
+config.font_size = 16
+config.font = wezterm.font("JetBrainsMono Nerd Font")
+
 return config
