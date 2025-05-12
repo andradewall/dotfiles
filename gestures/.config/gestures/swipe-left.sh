bc4f89be6734c185079e3526b1f04d034c17fac6
Wallace <andradewall0@gmail.com>
2025-05-12 16:49:10 -0300

    feat(gestures): finish next and prev scripts

--- gestures/.config/gestures/swipe-left.sh
+++ gestures/.config/gestures/swipe-left.sh
@@ -1,3 +1,15 @@
-#! /bin/bash
+#!/bin/bash
 
-hyprctl dispatch workspace 1
+# Get the current workspace ID
+workspace_id=$(hyprctl activeworkspace | grep "workspace ID" | awk '{print $3}')
+
+# Decrement the workspace ID by 1
+prev_workspace_id=$((workspace_id - 1))
+
+# Check if the previous workspace ID goes below the minimum of 1
+if [ "$prev_workspace_id" -lt 1 ]; then
+    prev_workspace_id=10
+fi
+
+# Dispatch to the previous workspace
+hyprctl dispatch workspace "$prev_workspace_id"
--- gestures/.config/gestures/swipe-right.sh
+++ gestures/.config/gestures/swipe-right.sh
@@ -1,3 +1,15 @@
-#! /bin/bash
+#!/bin/bash
 
-hyprctl dispatch workspace 2
+# Get the current workspace ID
+workspace_id=$(hyprctl activeworkspace | grep "workspace ID" | awk '{print $3}')
+
+# Increment the workspace ID by 1
+next_workspace_id=$((workspace_id + 1))
+
+# Check if the next workspace ID exceeds the cap of 10
+if [ "$next_workspace_id" -gt 10 ]; then
+    next_workspace_id=1
+fi
+
+# Dispatch to the next workspace
+hyprctl dispatch workspace "$next_workspace_id"
